52c824bde7370e270b9cc318877831537a11d431
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-07-04 12:12:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const tamanhoTela = window.matchMedia('(max-width: 1024px)');
+const menu = document.querySelector('.menu');
 
 if(tamanhoTela.matches){
-    const menu = document.querySelector('.menu');
     const hamburger = document.querySelector('.hamburger');
-
     hamburger.addEventListener('click', openMenu);
 
     function openMenu(){
         menu.classList.toggle('active');
     }
+
+
+  
     
 
 }
+
+
+
+  // scroll 
+
+
+
+  window.addEventListener('scroll', reduzMenu);
+
+
+  function reduzMenu(){
+      const windowTop = window.pageYOffset;
+      if((windowTop) > menu.offsetTop){
+          menu.classList.add('scroll');
+          console.log('oi')
+      } else{
+          menu.classList.remove('scroll');
+      }
+  }
